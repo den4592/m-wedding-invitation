c36c7f3ef6210f43fd2817b630d6bccf028523d6
den4592 <den4592@naver.com>
2023-07-05 16:07:09 +0900

    data가 없을 시 no data 문구 노출

--- client/src/components/GuestData.js
+++ client/src/components/GuestData.js
@@ -1,18 +1,24 @@
 const GuestData = ({ guestData }) => {
-  const td = guestData
-    .slice(0)
-    .reverse()
-    .map((data, index) => {
-      return (
-        <div key={index} className="guest-box">
-          <p className="from-text">From. {data.name}</p>
-          <hr />
-          <p className="text-text">{data.text}</p>
-        </div>
-      );
-    });
-
-  return <div className="guest-data">{td}</div>;
+  return (
+    <div className="guest-data">
+      {guestData.length ? (
+        guestData
+          .slice(0)
+          .reverse()
+          .map((data, index) => {
+            return (
+              <div key={index} className="guest-box">
+                <p className="from-text">From. {data.name}</p>
+                <hr />
+                <p className="text-text">{data.text}</p>
+              </div>
+            );
+          })
+      ) : (
+        <p style={{ textAlign: "center" }}>No Data.</p>
+      )}
+    </div>
+  );
 };
 
 export default GuestData;
